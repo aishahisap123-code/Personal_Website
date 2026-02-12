3094cf23ab248f771f929b1416b966b0a3d359fb
axisap <aishahisap123@gmail.com>
2026-02-12 12:13:19 +0000

    Update script.js
    changes which header is highlighted

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ document.addEventListener("mousemove", function(e) {
         trail.remove();
     }, 500);
 });
+
+let sections = document.querySelectorAll("sections");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.addEventListener("scroll", () =>{
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#"+ current) {
+            link.classList.add("active");
+        }
+    });
+});
